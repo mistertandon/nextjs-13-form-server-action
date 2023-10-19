a4c377e0e784cfa653f4aef18b2f6971233067ba
mistertandon <enggparveshtandon@gmail.com>
2023-10-19 09:11:22 +0530

    feat: Develop code to handle form POST request

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -3,6 +3,17 @@ import { NextResponse } from "next/server";
 const POST = async (request: Request) => {
   const data = await request.json();
   console.log("postAPI", data);
+  const res = await fetch("http://localhost:3004/todo", {
+    method: "post",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ id: Math.random(), title: data.title }),
+  });
+
+  const newTodo = await res.json();
+  console.log("newTodo", newTodo);
+
   return NextResponse.json({ message: `Todo has been added` });
 };
 
--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,4 +1,12 @@
-const ToDoPage = () => {
+import { revalidatePath } from "next/cache";
+
+const ToDoPage = async () => {
+  const todoRes = await fetch("http://localhost:3000/api/todo", {
+    cache: "no-store",
+  });
+
+  const todo = await todoRes.json();
+
   const addToDoHandler = async (formData: FormData) => {
     "use server";
 
@@ -10,10 +18,14 @@ const ToDoPage = () => {
       },
       body: JSON.stringify({ id: 1, title }),
     });
-    console.log("todosPage:before");
-    const todos = await fetch("http://localhost:3000/api/todo", { cache: 'no-store' });
-    const resToddo = await todos.json();
-    console.log("todosPage1", resToddo);
+
+    // const todos = await fetch("http://localhost:3000/api/todo", {
+    //   cache: "no-store",
+    // });
+
+    // const resToddo = await todos.json();
+    // console.log("todosPage1", resToddo);
+    revalidatePath("/todo");
   };
 
   return (
@@ -22,6 +34,9 @@ const ToDoPage = () => {
         <input name="title" type="text" />
         <button>SUbmit</button>
       </form>
+      {todo.map(({ title }: any, idx: number) => (
+        <div key={idx}>{title}</div>
+      ))}
     </>
   );
 };
